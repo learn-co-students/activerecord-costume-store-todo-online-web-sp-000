07ad352cf21f7cb27a059d2501c01a0b4b08abb4
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-03 08:34:36 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
-      t.string :description
+      t.string :description #spooky
     end
   end
 end
