43cdbce376bf57641971a3ca68924a6eac9b64c2
Rafael Batista <rafaelb24@yahoo.com>
2019-01-04 19:38:32 +0000

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,14 @@
-# Create your Costume class here
-# It should inherit from ActiveRecord::Base
+class Costume < ActiveRecord::Base 
+=begin
+    sql = <<-SQL 
+    CREATE TABLE IF NOT EXISTS costumes (
+      id INTEGER PRIMARY KEY, 
+      name TEXT,
+      price FLOAT,
+      size INTEGER,
+      image_url TEXT
+    )
+  SQL
+=end   
+ # ActiveRecord::Base.connection.execute(sql)
+end 
