60475caa0e88d93950bdb3285f723c50193303e4
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-13 20:51:31 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -42,8 +42,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.datetime "opening_date"
     t.datetime "closing_date"
     t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
