8b16ebbacdf59c04c954a653a5a73750c5b67f4f
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-13 20:50:34 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -9,7 +9,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.datetime :opening_date
       t.datetime :closing_date
       t.string :description
-      t.timestamps
+      #t.timestamps
     end
   end
-end 
+end
