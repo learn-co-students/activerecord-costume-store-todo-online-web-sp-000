8b3cffc85b7f880d911f14d22b1b81094100a9d3
Alison Reilly <reillyamr@gmail.com>
2019-03-06 17:39:36 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,9 @@
 # Create your costume_stores migration here
+
+class CreateCostumesStores < ActiveRecord::Base
+  def change 
+    create_table :costume_stores do |t|
+      t.string :name 
+      
+  
+end 
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,9 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Base
+  def change 
+    create_table :haunted_houses do |t|
+      t.string :name 
+      
+  
+end
