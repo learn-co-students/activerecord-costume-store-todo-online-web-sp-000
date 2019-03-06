153f31069803ce4544c27fc0cd6fa894d9ecdd3b
Alison Reilly <reillyamr@gmail.com>
2019-03-06 18:09:46 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,7 +3,13 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change 
     create_table :haunted_houses do |t|
-      t.string :name 
+      t.string :name
+      t.string :location
+      t.integer :price
+      t.string :theme 
+      t.boolean :family_friendly
+      t.datetime :opening_date 
+      t.datetime :closing_date
     end 
   end
       
