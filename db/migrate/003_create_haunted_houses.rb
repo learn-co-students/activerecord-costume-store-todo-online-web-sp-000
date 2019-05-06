99d68c72bc90bdb688deae175a31cac4c6dcfbc8
ADIL NAIMI <adilnm23@hotmail.com>
2019-05-06 06:51:37 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+        t.string :name
+        t.string :location
+        t.string :theme
+        t.integer :familyfriendly
+        t.integer :inbusiness
+        t.integer :openingdate
+        t.integer :closingtime
+        t.string :longdescription
+    end
+  end
+end
