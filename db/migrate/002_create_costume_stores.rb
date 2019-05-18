f02c25768100820d8618adf490fa9198703fb32c
Bry Cox <carl.cc@gmail.com>
2019-05-18 16:07:06 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,33 @@
 # Create your costume_stores migration here
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS costume_stores (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      location TEXT,
+      costume_inventory INTEGER,
+      num_of_employees INTEGER,
+      still_in_business TEXT
+      )
+    SQL
+
+    ActiveRecord::Base.connection.execute(sql)
+
+  end
+
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+    end
+  end
+
+
 end
