5ab56c63da47fb1ad29217c1e9596f0394ec5b7c
Frederic Silber <freddy.silber@cloudcoach.com>
2019-08-17 01:27:50 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,18 +9,6 @@
 # value types according to the spec
 
 class CreateCostumes < ActiveRecord::Migration[5.1]
-  def change
-  end
+  # def change
+  # end
 end
-#
-# class CreateCostumes < ActiveRecord::Migration[5.0]
-#   def change
-#     create_table :costumes do |t|
-#       t.string :name
-#       t.float :price
-#       t.string :image_url
-#       t.string :size
-#       t.timestamps
-#     end
-#   end
-# end
