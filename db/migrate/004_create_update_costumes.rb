655b98674282b4f26ace7e3af304f3772fa36003
Kirsten Keels <keelsk@gmail.com>
2020-03-21 01:54:35 +0000

    Done.

---
+++ db/migrate/004_create_update_costumes.rb
@@ -0,0 +1,6 @@
+class CreateUpdateCostumes < ActiveRecord::Migration[5.2]
+  def change
+    add_column :costumes, :created_at, :datetime
+    add_column :costumes, :updated_at, :datetime
+  end
+end
