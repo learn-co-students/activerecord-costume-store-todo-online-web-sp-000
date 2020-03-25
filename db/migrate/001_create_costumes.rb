9ccdaf69d724ac7ae874e362768118d0528eb9b7
Ronda D Cowen <cowenhome5@gmail.com>
2020-03-25 22:08:32 +0000

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -4,8 +4,8 @@ def change
   create_table :costumes do |t|
     t.string :name
     t.float :price
-    t.integer :size
-    t.string :image
+    t.string :size
+    t.text :image_url
     t.timestamps
     end
   end
