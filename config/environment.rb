eb57fa8a4165af85e646547bf453d5ddc3ee802e
nathan-stamand <nathansaint810@gmail.com>
2020-03-31 08:10:38 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -9,4 +9,7 @@ Bundler.require
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 
 connection_details = YAML::load(File.open('config/database.yml'))
-ActiveRecord::Base.establish_connection(connection_details)
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/halloween.db"
+)
