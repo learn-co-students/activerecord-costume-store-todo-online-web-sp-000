7642d3c79536c29917635f781623a0105899bcdd
Taishawnk <Taishawnking1@yahoo.com>
2020-05-03 19:50:06 -0600

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,12 +1,12 @@
-class CostumeStores < ActiveRecord
+class CostumeStores < ActiveRecord[5.1]
     def change 
         create_table :costume_stores do |t|
-            t.string :name
-            t.string :location
+            t.string  :name
+            t.string  :location
             t.integer :costume_inventory
             t.integer :num_of_employees
             t.boolean :still_in_business
-            t.text   :open_time
+            t.text    :open_time
             t.text    :close_time
         end
     end
