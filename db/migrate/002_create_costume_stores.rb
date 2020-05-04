10c337a8d9507fe3ce8a5f09a6af97faacae9a34
Taishawnk <Taishawnking1@yahoo.com>
2020-05-03 19:05:06 -0600

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,13 @@
-# Create your costume_stores migration here
+class CostumeStores < ActiveRecord
+    def change 
+        create_table :costume_stores do |t|
+            t.string :name
+            t.string :location
+            t.integer :costume_inventory
+            t.integer :num_of_employees
+            t.boolean :still_in_business
+            t.text   :open_time
+            t.text    :close_time
+        end
+    end
+end
