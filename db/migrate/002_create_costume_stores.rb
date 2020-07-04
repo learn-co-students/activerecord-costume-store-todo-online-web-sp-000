07c66433c2f58679016a8977e2db277af0671b11
Cody Alvarez <cody_alvarez19@outlook.com>
2020-07-04 03:19:46 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
+class CostumeStore < ActiveRecord::Migration[4.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+
+      t.timestamps
+
+    end
+  end
+end
 # Create your costume_stores migration here
