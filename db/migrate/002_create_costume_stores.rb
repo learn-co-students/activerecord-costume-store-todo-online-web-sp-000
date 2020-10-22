817270f928b2e0693339d2c977fdf15fed39160c
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2020-10-22 15:02:08 -0500

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+    def change
+    create_table :costume_stores do |c|
+        c.string :name
+        c.string :location
+        c.string :costume_inventory
+        c.integer :num_of_employees
+        c.boolean :still_in_business
+        c.timestamp :opening_time
+        c.timestamp :closing_time
+        end
+    end
+end
