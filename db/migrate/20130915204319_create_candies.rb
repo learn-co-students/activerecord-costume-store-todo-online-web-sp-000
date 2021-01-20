490731e04ce4b34be3493515f4ac1f0fd7f7cd58
jasonronalddavis <jdavis@c.ringling.edu>
2021-01-20 15:33:52 -0600

    Done.

--- db/migrate/20130915204319_create_candies.rb
+++
@@ -1,9 +0,0 @@
-class CreateCandies < ActiveRecord::Migration[5.1]
-    def change
-      create_table :candies do |t|
-        t.string :name
-        t.integer :calories
-        t.timestamps
-      end
-    end
-  end
