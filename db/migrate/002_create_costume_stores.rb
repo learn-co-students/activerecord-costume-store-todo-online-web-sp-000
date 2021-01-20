37ecbd877bc81acbcf936a4cad612cedcbb6f85d
jasonronalddavis <jdavis@c.ringling.edu>
2021-01-20 03:34:49 -0600

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
-# Create your costume_stores migration here
+# Create a class and inherit from ActiveRecord::Migration
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+
+    def change 
+        create_table costume_store do |c|
+        c.datetime :opening_time
+        c.string :name
+        c.string :location
+        c.integer :employees
+        c.integer :costume_inventory
+        c.boolean :still_in_business
+        c.datetime :closing_time
+    end
+end
+end
