932ba2aaf54955afc4e7799bfb8b0ceca4abb7c6
Jordan Reed <crasinator@gmail.com>
2021-04-02 19:53:01 -0400

    Done.

---
+++ db/migrations/001_create_costumes.rb
@@ -0,0 +1,10 @@
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change
+      create_table :costumes do |t|
+        t.string :name 
+        t.float :price
+        t.integer :size 
+        t.string :image_url
+      end
+  end 
+end 
